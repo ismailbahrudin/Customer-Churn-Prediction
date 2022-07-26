ba1b7bc1bd9f7c0d9c3f88d47cb40a2b3b36db65
ismailbahrudin <109711153+ismailbahrudin@users.noreply.github.com>
2022-07-27 00:08:43 +0800

    Update churn.py

--- churn.py
+++ churn.py
@@ -26,7 +26,7 @@ st.subheader('User Input parameters')
 st.write(df)
 
 churn = pd.read_csv('https://raw.githubusercontent.com/ismailbahrudin/Discount-Profit/3745beb6bba0461ce7644161839bfdbd99232625/data.csv')
-X = churn['Score','Age','Tenure']
+X = churn.drop(['Row','Id','Surname','Nationality','Gender','Balance','Products','Card','Active','Salary','Exited'],axis=1)
 Y = churn['Exited']
 
 clf = RandomForestClassifier()
