87cf555afc58ac40f97a7e68379ab35974277573
ismailbahrudin <109711153+ismailbahrudin@users.noreply.github.com>
2022-07-27 00:14:23 +0800

    Update churn.py

--- churn.py
+++ churn.py
@@ -36,7 +36,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-dataf= pd.DataFrame(['Churn','Not Churn',])
+dataf= pd.DataFrame(['Not Churn','Churn',])
 st.write(dataf)
 
 st.subheader('Prediction')
